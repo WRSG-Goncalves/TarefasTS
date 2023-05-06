6203e70785fdb10134f7ddbfdde2ec0a00eebd6c
WRSG-Goncalves <wilssp@gmail.com>
2023-05-06 15:51:28 -0300

    TypeScript, interface, class, enum.

---
+++ src/interface/taskInterface.ts
@@ -0,0 +1,8 @@
+ interface Task {
+    id: string;
+    dateCreated: Date;
+    dateUpdated: Date;
+    description: string;
+    render(): string;
+  }
+
---
+++ src/model/reminder.ts
@@ -0,0 +1,21 @@
+// import {Task} from "../interface/taskInterface";
+
+// export class Reminder implements Task{
+//     id: string = '';
+//     dateCreated: Date = new Date();
+//     dateUpdated: Date = new Date();
+//     description: string = '';
+    
+//     date: Date = new Date();
+//     notification: Array<string> = ['EMAIL'];
+//     constructor(description:string, date: Date, notification: Array<string>) {
+//         this.description = description;
+//         this.date = date;
+//         this.notification = notification;
+//     }
+    
+//     render(): string {
+//         return JSON.stringify(this);
+//     }
+
+// }
